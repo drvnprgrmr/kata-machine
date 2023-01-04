1ee894d952c44ff18f4322cd6526768a3db567ec
Replit user <>
2023-01-04 16:45:53 +0000

    Day1 BinarySearchList completed

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let hi = haystack.length
+    let lo = 0
 
+    do {
+        let mid = Math.floor(lo + (hi - lo) / 2)
+        const v = haystack[mid]
+        
+        if (v === needle)  return true
+        else if (v > needle) hi = mid
+        else lo = mid + 1
+        
+    } while (lo < hi)
+
+    return false
 }
