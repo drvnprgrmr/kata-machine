f4cb516ec8317cedca533aeddc822159b0390120
Ibukun Ekunwe <67306305+drvnprgrmr@users.noreply.github.com>
2023-01-04 17:27:56 +0000

    Day1 TwoCrystalBalls completed and  resumed tracking of replit config files.

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,19 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // Initialize the jump amount 
+    let jmpAmount = Math.floor(Math.sqrt(breaks.length))
 
+    // Start at the first jump
+    let i = jmpAmount
+    for (; i < breaks.length; i += jmpAmount) {
+        // If first ball breaks; break from the loop
+        if (breaks[i]) break
+    }
+    // Move back by the jump amount and
+    i -= jmpAmount
+    
+    for (let j=0; j < jmpAmount && i < breaks.length; i++, j++) {
+        if (breaks[i]) return i
+    }
+
+    return -1
 }
